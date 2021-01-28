ce0149917c8d389710136d9bd3667096d3ccb718
Nikita <69010558+Kapibarra@users.noreply.github.com>
2021-01-28 11:07:18 +0300

    added overlay for mobile width

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
 import './scss/index.scss'
+
+
+$("#navToggle").click(function() {
+    $(this).toggleClass("active");
+    $(".overlay").toggleClass("open");
+    // this line ▼ prevents content scroll-behind
+    $("body").toggleClass("locked");
+});
+
+$(".overlay a").click(function() {
+    $("#navToggle").toggleClass("active");
+    $(".overlay").toggleClass("open");
+    $("body").toggleClass("locked");
+});
